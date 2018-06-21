5fe8fe5cfbab9f61b7241dcb06d874a56b5f9bd2
prprprprbiu <869720477@qq.com>
2018-06-21 15:44:39 +0800

    111

---
+++ helloclass.cpp
@@ -0,0 +1,13 @@
+#include "helloclass.h"
+#include <iostream>
+
+using namespace std;
+
+HelloClass::HelloClass(){
+      cout << "I am being created."<<endl;}
+
+HelloClass::~HelloClass(){
+      cout << "I am being destroyed." << endl;}
+
+void HelloClass::sayHello(){
+      cout << "Hello Class!" <<endl;}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "helloclass.h"
+
+using namespace std;
+
+int main(){
+    HelloClass a;
+    a.sayHello();
+    return 0;}
